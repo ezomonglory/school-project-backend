799fb2339a860d43cc8368022bbc109c162019d2
Ezomon Glory <ezomonglory01@gmail.com>
2023-09-13 03:56:03 +0100

    kk

--- Route/Course.js
+++ Route/Course.js
@@ -125,142 +125,170 @@ router.post("/add-course", async (req, res, next) => {
 
         const result = req.body;
 
-if (result.role === "student") {
-    const studentInfo = {
-        name: result.studentName, // Replace with the actual student name
-        // Add other student information fields as needed
-    };
+        if (result.role === "student") {
+            const studentInfo = {
+                        name: result.full_name, // Replace with the actual student name
+                        matric_number: result.identity_number,
+                    };
 
-    const courseIds = result.courses.map((course) => course.id);
+            const courseIds = result.courses.map((course) => course.id);
 
-    // Assuming you have a User model
-    User.findOneAndUpdate(
-        { identity_number: result.identity_number }, // Assuming userId is available in the request
-        { $set: studentInfo },
-        { new: true }
-    )
-        .then((updatedUser) => {
-            if (!updatedUser) {
-                // Handle the case when the user is not found
-                res.status(404).send("User not found");
-                return;
-            }
-
-            // Now that the user details are updated, proceed to update the courses
-
-            // Assuming you have a Course model
-            Course.find({ _id: { $in: courseIds } })
-                .then((courses) => {
-                    if (!courses || courses.length === 0) {
-                        // Handle the case when no courses are found
-                        res.status(404).send("No courses found");
+            // Assuming you have a User model
+            User.findOneAndUpdate(
+                { identity_number: result.identity_number }, // Assuming userId is available in the request
+                { $set: studentInfo },
+                { new: true }
+            )
+                .then((updatedUser) => {
+                    if (!updatedUser) {
+                        // Handle the case when the user is not found
+                        res.status(404).send("User not found");
                         return;
                     }
 
-                    const alreadyRegisteredCourses = [];
+                    // Now that the user details are updated, proceed to update the courses
 
-                    // Check if the student is already in the students array of any course
-                    courses.forEach((course) => {
-                        const isStudentRegistered = course.students.some((student) => {
-                            return student.name === studentInfo.name; // Check based on a unique identifier, like name
-                        });
+                    // Assuming you have a Course model
+                    Course.find({ _id: { $in: courseIds } })
+                        .then((courses) => {
+                            if (!courses || courses.length === 0) {
+                                // Handle the case when no courses are found
+                                res.status(404).send("No courses found");
+                                return;
+                            }
 
-                        if (isStudentRegistered) {
-                            alreadyRegisteredCourses.push(course.name); // Store the course name in which the student is already registered
-                        } else {
-                            course.students.push(studentInfo); // Add the student to the course if not already registered
-                            
-                            // Add course code and course ID to the user's course array
-                            updatedUser.courses.push({ code: course.code, id: course._id });
-                        }
-                    });
+                            const alreadyRegisteredCourses = [];
 
-                    if (alreadyRegisteredCourses.length > 0) {
-                        // Handle the case when the student is already registered in some courses
-                        res.status(400).send(`Student is already registered in courses: ${alreadyRegisteredCourses.join(', ')}`);
-                    } else {
-                        // Save the updated courses and user
-                        const savePromises = [...courses.map((course) => course.save()), updatedUser.save()];
-                        
-                        Promise.all(savePromises)
-                            .then(() => {
-                                // Send a success response
-                                res.status(200).send("User and courses updated successfully");
-                            })
-                            .catch((err) => {
-                                // Handle the error if the update fails
-                                console.error(err);
-                                res.status(500).send("Failed to update courses");
+                            // Check if the student is already in the students array of any course
+                            courses.forEach((course) => {
+                                const isStudentRegistered = course.students.some((student) => {
+                                    return student.name === studentInfo.name; // Check based on a unique identifier, like name
+                                });
+
+                                if (isStudentRegistered) {
+                                    alreadyRegisteredCourses.push(course.name); // Store the course name in which the student is already registered
+                                } else {
+                                    course.students.push(studentInfo); // Add the student to the course if not already registered
+
+                                    // Add course code and course ID to the user's course array
+                                    updatedUser.courses.push({ code: course.code, id: course._id });
+                                }
                             });
-                    }
+
+                            if (alreadyRegisteredCourses.length > 0) {
+                                // Handle the case when the student is already registered in some courses
+                                res.status(400).send(`Student is already registered in courses: ${alreadyRegisteredCourses.join(', ')}`);
+                            } else {
+                                // Save the updated courses and user
+                                const savePromises = [...courses.map((course) => course.save()), updatedUser.save()];
+
+                                Promise.all(savePromises)
+                                    .then(() => {
+                                        // Send a success response
+                                        res.status(200).send("User and courses updated successfully");
+                                    })
+                                    .catch((err) => {
+                                        // Handle the error if the update fails
+                                        console.error(err);
+                                        res.status(500).send("Failed to update courses");
+                                    });
+                            }
+                        })
+                        .catch((err) => {
+                            // Handle the error
+                            console.error(err);
+                            res.status(500).send("Internal Server Error");
+                        });
                 })
                 .catch((err) => {
                     // Handle the error
                     console.error(err);
                     res.status(500).send("Internal Server Error");
                 });
-        })
-        .catch((err) => {
-            // Handle the error
-            console.error(err);
-            res.status(500).send("Internal Server Error");
-        });
-} else {
-    // Handle the case when the role is not "Student"
-    res.status(400).send("Invalid role");
-}
-
-
+        }
 
         if (result.role === "teacher") {
             const studentInfo = {
                 name: result.full_name, // Replace with the actual student name
                 staff_id: result.identity_number,
+
+
             };
 
             const courseIds = result.courses.map((course) => course.id);
 
-            // Assuming you have a Course model
-            Course.find({ _id: { $in: courseIds } })
-                .then((courses) => {
-                    if (!courses || courses.length === 0) {
-                        // Handle the case when no courses are found
-                        res.status(404).send("No courses found");
+            // Assuming you have a User model
+            User.findOneAndUpdate(
+                { identity_number: result.identity_number }, // Assuming userId is available in the request
+                { $set: studentInfo },
+                { new: true }
+            )
+                .then((updatedUser) => {
+                    if (!updatedUser) {
+                        // Handle the case when the user is not found
+                        res.status(404).send("User not found");
                         return;
                     }
 
-                    const alreadyRegisteredCourses = [];
+                    // Now that the user details are updated, proceed to update the courses
 
-                    // Check if the student is already in the students array of any course
-                    courses.forEach((course) => {
-                        const isStudentRegistered = course.lecturers.some((student) => {
-                            return student.staff_id === studentInfo.staff_id; // Check based on a unique identifier, like name
-                        });
+                    // Assuming you have a Course model
+                    Course.find({ _id: { $in: courseIds } })
+                        .then((courses) => {
+                            if (!courses || courses.length === 0) {
+                                // Handle the case when no courses are found
+                                res.status(404).send("No courses found");
+                                return;
+                            }
 
-                        if (isStudentRegistered) {
-                            alreadyRegisteredCourses.push(course.course_code); // Store the course name in which the student is already registered
-                        } else {
-                            course.lecturers.push(studentInfo); // Add the student to the course if not already registered
-                        }
-                    });
+                            const alreadyRegisteredCourses = [];
 
-                    if (alreadyRegisteredCourses.length > 0) {
-                        // Handle the case when the student is already registered in some courses
-                        res.status(400).send(`Teacher is already registered in courses: ${alreadyRegisteredCourses.join(', ')}`);
-                    } else {
-                        // Save the updated courses
-                        Promise.all(courses.map((course) => course.save()))
-                            .then(() => {
-                                // Send a success response
-                                res.status(200).send("Courses updated successfully");
-                            })
-                            .catch((err) => {
-                                // Handle the error if the update fails
-                                console.error(err);
-                                res.status(500).send("Failed to update courses");
+                            // Check if the student is already in the students array of any course
+                            courses.forEach((course) => {
+                                const isStudentRegistered = course.lecturers.some((student) => {
+                                    return student.name === studentInfo.name; // Check based on a unique identifier, like name
+                                });
+
+                                if (isStudentRegistered) {
+                                    alreadyRegisteredCourses.push(course.name); // Store the course name in which the student is already registered
+                                } else {
+                                    course.students.push(studentInfo); // Add the student to the course if not already registered
+
+                                    // Add course code and course ID to the user's course array
+                                    updatedUser.courses.push({ code: course.code, id: course._id });
+                                }
                             });
-                    }
+
+                            if (alreadyRegisteredCourses.length > 0) {
+                                // Handle the case when the student is already registered in some courses
+                                res.status(400).send(`Student is already registered in courses: ${alreadyRegisteredCourses.join(', ')}`);
+                            } else {
+                                // Save the updated courses and user
+                                const savePromises = [...courses.map((course) => course.save()), updatedUser.save()];
+
+                                Promise.all(savePromises)
+                                    .then(() => {
+                                        // Send a success response
+                                        res.status(200).send("User and courses updated successfully");
+                                    })
+                                    .catch((err) => {
+                                        // Handle the error if the update fails
+                                        console.error(err);
+                                        res.status(500).send("Failed to update courses");
+                                    });
+                            }
+                        })
+                        .catch((err) => {
+                            // Handle the error
+                            console.error(err);
+                            res.status(500).send("Internal Server Error");
+                        });
                 })
+                .catch((err) => {
+                    // Handle the error
+                    console.error(err);
+                    res.status(500).send("Internal Server Error");
+                });
         }
     } catch (err) {
         next(err)
